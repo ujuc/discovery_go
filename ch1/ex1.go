cd297a1fe5ca90850f905cd9d90b6e393b6a85fa
Sungjin Kang <ujuc@ujuc.kr>
2016-12-06 01:44:07 +0900

    chapter 1 - ex1

---
+++ ch1/ex1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func print(n int) {
+	for i := 1; i <= n; i++ {
+		n1 := i * 10
+		n2 := (i + 1) * 10
+
+		s := fmt.Sprintf("타잔이 %d 원짜리 팬티를 입고, %d 원짜리 칼을 차고 노래를 한다.", n1, n2)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	print(10)
+}
