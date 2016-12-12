b2c0c0ed3c68bb7865c2134f0479c597a29cfdea
Sungjin Kang <ujuc@ujuc.kr>
2016-12-12 22:39:06 +0900

    Add ch3 예제 작성

---
+++ ch3/maps/set/set.go
@@ -0,0 +1,13 @@
+package set
+
+func hasDupeRune(s string) bool {
+	runeSet := map[rune]struct{}{}
+
+	for _, r := range s {
+		if _, exists := runeSet[r]; exists {
+			return true
+		}
+		runeSet[r] = struct{}{}
+	}
+	return false
+}
